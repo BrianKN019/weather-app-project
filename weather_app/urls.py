63db3eb4f2a8c55763c1d2e778f2ecbe13d82429
BRIAN KIMEMIA NJERI <briannkimemia@gmail.com>
2025-02-19 23:34:15 +0300

    Update urls.py

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -20,4 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('weather.urls')),  # Include the URLs from the weather app
-]
+]   
